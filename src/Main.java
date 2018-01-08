2dffebdc140e58d96ef8ff7eb54c311e4289ef02
Samuel Kopp <samuel.r.kopp@gmail.com>
2018-01-08 14:11:08 +0100

    FX

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
