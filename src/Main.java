3c741ae020499c98bbe6acd1f1147ad0e9c60925
Samuel Kopp <samuel.r.kopp@gmail.com>
2018-01-08 14:13:13 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * This class initializes and starts the JavaFX application. 
+ * 
+ * @author Jordan, Basia, Blazej, Yvar, Stijn, Samuel
+ */
 public class Main extends Application{
-
-	@Override
+	
+	// VERSION
+	public static final String VERSION = "1.2"; 
+	
+//	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
 		
