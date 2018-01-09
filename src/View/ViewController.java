6af239ac9b3396df557c8ad96d6c8523b24df708
Samuel Kopp <samuel.r.kopp@gmail.com>
2018-01-09 18:31:31 +0100

    Controller setup under process

--- src/View/ViewController.java
+++ src/View/ViewController.java
@@ -1,3 +1,24 @@
 package View;
+
+import Model.ContainerModel;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
 public class ViewController{
+	
+	private final ContainerModel model;
+	
+	private Stage primaryStage;
+	
+	public ViewController(ContainerModel model) {
+		this.model = model;
+		primaryStage = Main.getPrimaryStage();
+		
+		allowCameraHandler();
+	}
+	
+	public void allowCameraHandler() {
+		_primaryStage.getScene().setOnKeyPressed(new EventHandler<KeyEvent>()
+	}
 }
