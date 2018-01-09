889ccf65718df07f673e013e2f5e69e1e4bbc6d7
Samuel Kopp <samuel.r.kopp@gmail.com>
2018-01-09 18:02:43 +0100

    Camera

--- src/View/ContainerPane.java
+++ src/View/ContainerPane.java
@@ -1,21 +1,24 @@
 package View;
 
 import javafx.geometry.Insets;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.DrawMode;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 
 public class ContainerPane extends Pane {
 	
-	private double CONTAINER_LENGTH = 16.5;
-	private double CONTAINER_WIDTH = 2.5;
-	private double CONTAINER_HEIGHT = 4.0;
+	private double CONTAINER_LENGTH = 16.5*10;
+	private double CONTAINER_WIDTH = 2.5*10;
+	private double CONTAINER_HEIGHT = 4.0*10;
 	
 	private Rotate xAxis = new Rotate(0, Rotate.X_AXIS);
     private Rotate yAxis = new Rotate(0, Rotate.Y_AXIS);
@@ -25,19 +28,33 @@ public class ContainerPane extends Pane {
     private double angleZ = 0;
     
     public ContainerPane() {
-    	setPrefSize(200,200);
+    	setPrefSize(400,400);
+    	
+    	// Create and position camera
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.getTransforms().addAll(
+                xAxis, yAxis, zAxis, new Translate(0, 0, -50));
     	
     	//shifts the origin of the space 
     	setTranslateX(200);
     	setTranslateY(200);
     	
     	//The 3 Boxes 
-    	Box ground = new Box(CONTAINER_WIDTH , 0, CONTAINER_LENGTH);
-    	Box side = new Box(0, CONTAINER_HEIGHT, CONTAINER_LENGTH);
-    	Box back = new Box(CONTAINER_WIDTH,CONTAINER_HEIGHT, 0);
+    	Box ground = new Box(CONTAINER_WIDTH , CONTAINER_HEIGHT, CONTAINER_LENGTH);
+    	//Box side = new Box(CONTAINER_WIDTH, CONTAINER_HEIGHT, CONTAINER_LENGTH);
+    	//Box back = new Box(CONTAINER_WIDTH,CONTAINER_HEIGHT, CONTAINER_LENGTH);
     	
+    	ground.setDrawMode(DrawMode.FILL);
+    	//side.setDrawMode(DrawMode.FILL);
+    	//back.setDrawMode(DrawMode.FILL);
+    	
+    	ground.setMaterial(new PhongMaterial(Color.LIGHTPINK));
+    	//side.setMaterial(new PhongMaterial(Color.RED));
+    	//back.setMaterial(new PhongMaterial(Color.ORANGE));
+    	
+    	getChildren().add(camera);
     	getChildren().add(ground);
-    	getChildren().add(side);
-    	getChildren().add(back);
+    	//getChildren().add(side);
+    	//getChildren().add(back);
     }
 }
