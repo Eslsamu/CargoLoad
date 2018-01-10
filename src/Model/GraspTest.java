9cfffd8295a799765134ec8481fe97643fdb5272
yvar <yvarhulshof@hotmail.com>
2018-01-10 16:23:45 +0100

    Grasp changes

--- src/Model/GraspTest.java
+++ src/Model/GraspTest.java
@@ -2,5 +2,9 @@ package Model;
 
 public class GraspTest {
 
+    public static void main(String[] args){
+        Grasp graspModel = new Grasp();
+        graspModel.initialize(10,10,10);
 
+    }
 }
