828e836063113c210862d81dc41daab616eb756e
Samuel Kopp <samuel.r.kopp@gmail.com>
2018-01-20 12:03:28 +0100

    my testclass for maximum space alg

--- src/Test/TestField.java
+++ src/Test/TestField.java
@@ -3,18 +3,16 @@ package Test;
 import java.util.ArrayList;
 
 public class TestField {
-	 boolean[][] container = new boolean[10][10];
+	 boolean[][] container = new boolean[6][6];
 	 ArrayList<SubSpace> maxSpaces = new ArrayList<SubSpace>();
 	 
 	 public static void main(String[] args) {
 		 TestField testF = new TestField();
-		 testF.container[3][3]=true;
-		 testF.container[4][2]=true;
-		 testF.container[0][5]=true;
+		 testF.container[4][4]=true;
+		 testF.container[3][5]=true;
+
 		 
-		 testF.container[1][4]=true;
-		 
-		 testF.findMaxSpaces(0, 0);
+		 testF.find(0, 0);
 		 testF.printList();
 	 }
 	 
@@ -41,6 +39,8 @@ public class TestField {
 		 int vY = container[0].length;
 		 
 		 //into y direction 
+		 
+		 
 		 for(int x = startX; x < vX; x++) {
 			 if(x == vX-1 && vY > 0) maxSpaces.add(new SubSpace(x+1, vY, startX,startY));
 			 for(int y = startY; y < vY; y++) {
@@ -48,13 +48,40 @@ public class TestField {
 					 if(x > 0) {
 						 maxSpaces.add(new SubSpace(x,vY,startX,startY));
 					 }
-					 if(y < 0) break;
+					 if(y <= 0) break;
 					 vY = y;
 				 }
 				 
 			 }
 		 }
 	 }
+	 
+	 public void find(int startX, int startY) {
+		 int xMax = container.length;
+		 int yMax = container[0].length;
+		 int pivotY = yMax; 
+
+		 maxSpaces.add(new SubSpace(xMax,yMax,startX, startY));
+		 for(int x = 0; x < xMax; x++) {
+			 for(int y = 0; y < pivotY; y++) {
+				 if(container[x][y]) {
+					if(x==0) {
+						maxSpaces.remove(maxSpaces.size()-1);
+						}
+					else{
+					maxSpaces.get(maxSpaces.size()-1).vectorX = x;
+					maxSpaces.get(maxSpaces.size()-1).vectorY = pivotY;
+					}
+					
+					pivotY = y;
+					
+					if(y>0) maxSpaces.add(new SubSpace(xMax, y, startX, startY));
+					break;
+				 }
+			 }
+		 }
+	 }
+
 	 
 	 public void printList() {
 		 for(SubSpace s : maxSpaces) {
